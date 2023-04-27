cd7e87a9c33497960d44659b3524da2859d61cea
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2023-04-27 10:01:26 -0600

    resource card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
     <TheHeader />
+    <ul v-for="resource in storedResources" :key="resource.id">
+        <ResourceCard :id="resource.id" :title="resource.title" :description="resource.description" :link="resource.link" @delete-resource="removeResource"/>
+    </ul>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
+import ResourceCard from './components/ResourceCard.vue'
+
 export default {
     components: {
-        TheHeader
+        TheHeader,
+        ResourceCard,
+    },
+    data(){
+        return {
+            storedResources: [
+                {
+                    id: '1',
+                    title: 'Official Guide',
+                    description: 'The Official Vue.js documentation',
+                    link: 'https://vuejs.org/guide/introduction.html'
+                },
+                {
+                    id: '2',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://vuejs.org/guide/introduction.html'
+                },
+            ]
+        }
+    },
+    methods: {
+        removeResource(id) {
+            let selectedResource = this.storedResources.filter(e=>e.id===id)
+            console.log('remove ', selectedResource)
+        }
     }
 }
 </script>
---
+++ src/components/ResourceCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <li class="resource__card">
+        <h2 class="resource__title"> {{ title }} </h2>
+        <p class="resource__desc"> {{ description }} </p>
+        <a :href=link class="resource__link">View Resource</a>
+        <button @click="deleteResource(id)">Delete</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    emits: {
+        'delete-resource': function(id) {
+            if(id) {
+                return true
+            } else {
+                console.log('resource id is missing')
+                return false
+            }
+        }
+    },
+    methods: {
+        deleteResource(){
+            this.$emit('delete-resource', this.id)
+        }
+    }
+}
+</script>
